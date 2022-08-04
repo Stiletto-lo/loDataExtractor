e64c2f27ffc94f6a51010ffcee07d9a92853b916
dm94 <dm94@hotmail.es>
2022-08-04 16:43:54 +0200

    Add more loot sites

--- translations/lootSites.js
+++ translations/lootSites.js
@@ -8,10 +8,54 @@ lootSites["LootBoxTablets"] = "Tablets Strongbox";
 lootSites["LootBoxTools"] = "Tools Strongbox";
 lootSites["LootBoxWeapons"] = "Weapons Strongbox";
 
-lootSites["RupuShaman_T2"] = "Rupu Shaman";
-lootSites["RupuShaman_T3"] = "Rupu Shaman";
 lootSites["MediumLargeRupuCamp_01_2"] = "Large Rupu Camp - Large Chest";
 lootSites["MediumLargeRupuCamp_01_1"] = "Large Rupu Camp - Large Chest";
 lootSites["MediumLargeRupuCamp_01"] = "Large Rupu Camp - Small Chest";
+lootSites["CanyonNurrRaider"] = "Canyon Map - Nurr Raider";
+lootSites["CanyonNurrRaiderCamp"] = "Canyon Map - Nurr Raider Camp";
+lootSites["RupuShaman_T2"] = "Rupu Shaman";
+lootSites["RupuShaman_T3"] = "Rupu Shaman";
+lootSites["RupuShaman"] = "Rupu Shaman";
+lootSites["RupuForager"] = "Rupu Forager";
+lootSites["LightningRupu"] = "White Death";
+lootSites["RupuLoot_T0"] = "All rupus";
+lootSites["RupuLoot_T1"] = "All rupus";
+lootSites["RupuLoot_T2"] = "All rupus";
+lootSites["RupuThug"] = "Rupu Thug";
+lootSites["RupuUrnLoot_T0"] = "Rupu Urn";
+lootSites["RupuScavenger"] = "Rupu Scavenger";
+lootSites["RupuScavenger_T2"] = "Rupu Scavenger";
+lootSites["RupuScavenger_T3"] = "Rupu Scavenger";
+lootSites["RupuHazraki_T2"] = "Rupu Hazraki";
+lootSites["RupuHazraki_T3"] = "Rupu Hazraki";
+lootSites["RupuHarasser_T0"] = "Rupu Harasser";
+lootSites["RupuForerunner"] = "Rupu Forerunner";
+lootSites["RupuForerunner_T2"] = "Rupu Forerunner";
+lootSites["RupuForerunner_T3"] = "Rupu Forerunner";
+lootSites["RupuSentinel"] = "Rupu Sentinel";
+lootSites["RupuSentinel_T2"] = "Rupu Forerunner";
+lootSites["RupuSentinel_T3"] = "Rupu Forerunner";
+lootSites["RupuFirebrand_T3"] = "Rupu Firebrand";
+lootSites["RupuDrudgeSentry_T3"] = "Rupu Drudge";
+lootSites["NativeRupu"] = "Rupu Hermit";
+lootSites["IllusiveGiantRupu"] = "Yellow Rupu";
+
+lootSites["CanyonIslandShelterUrn"] = "Canyon Islands Map - Shelter Urn";
+lootSites["CanyonIslandShelterImproveUrn"] = "Canyon Islands Map - Shelter Urn";
+
+lootSites["CanyonSavannahCampChest"] = "Canyon Map - Camp Chest";
+
+lootSites["CanyonForagingVillageUrn"] = "Canyon Map - Village Camp";
+lootSites["CanyonForagingVillageChest"] = "Canyon Map - Village Camp";
+lootSites["CanyonMainVillageUrn"] = "Canyon Map - Village Camp";
+
+lootSites["CanyonPOIDragableChest"] = "Canyon Map - Ruin - Dragable Chest";
+lootSites["CanyonPOIBurnableChest"] = "Canyon Map - Ruin - Burnable Chest";
+
+lootSites["CanyonEndCampChest"] = "Canyon Map - Ruin Camp";
+
+lootSites["CradleFortifiedCamp"] = "Cradle Map - Fortified Camp";
+lootSites["PoiCradleChestLoot_T0"] = "Cradle Map - Ruin";
+lootSites["PoiMediumCradleHangingBagLoot_T0"] = "Cradle Map - Ruin";
 
 module.exports = lootSites;
